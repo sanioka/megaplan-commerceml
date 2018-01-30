44c208bb2bf5258512e6e919dc240f590cc56cbb
sanioka <isanioka@gmail.com>
2018-01-31 01:37:21 +0300

    minor changes

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Example to import leads from site/landing to megaplan
 https://help.megaplan.ru/API_bibles
 
 https://help.megaplan.ru/API_online_store
+
+1. Setup auth data 
+
+/resourses/params.php
+(see example file).
+
+2. Run CommerceML intergation:
+
+/example-commerceml.php
---
+++ example-api-integration.php
@@ -0,0 +1,46 @@
+<?php
+
+/**************************************************/
+// Megaplan API example
+// Пример создания сделки с использованием формата CommerceML
+/**************************************************/
+
+// Подключаем библиотеку API
+include('resources/request.php');
+include('resources/params.php');
+
+// Авторизуемся в Мегаплане
+$request = new SdfApi_Request('', '', $host, true);
+$response = json_decode(
+    $request->get(
+        '/BumsCommonApiV01/User/authorize.api',
+        array(
+            'Login' => $login,
+            'Password' => md5($password)
+        )
+    )
+);
+
+// Получаем AccessId и SecretKey
+$accessId = $response->data->AccessId;
+$secretKey = $response->data->SecretKey;
+
+// Переподключаемся с полученными AccessId и SecretKey
+unset($request);
+$request = new SdfApi_Request($accessId, $secretKey, $host, true);
+
+
+// Формируем CommerceML-документ
+$commerceML = generateCommerceML();
+
+// Создаем сделку в Мегаплане
+$result = $request->post('/BumsTradeApiV01/Deal/createFromOnlineStore.api', array('CommerceInfo' => $commerceML));
+echo($result);
+
+function generateCommerceML()
+{
+    $commerceML = include('view/commerceML.php');
+    return $commerceML;
+}
+
+
